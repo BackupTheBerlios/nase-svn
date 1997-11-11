e75542a90a71f3abff33b3bf33fb8ae96637f408
saam <saam@8aa35c3b-e50b-0410-9e69-e2dc56048116>
1997-11-11 15:03:03 +0000

    *** empty log message ***

---
+++ nase/trunk/nasedirs.bash
@@ -0,0 +1,4 @@
+export NASEDIRS=${NASEPATH}/control:${NASEPATH}/graphic:${NASEPATH}/graphic/nonase:${NASEPATH}/input:${NASEPATH}/misc:${NASEPATH}/misc/array:${NASEPATH}/misc/structures:${NASEPATH}/simu/connections:${NASEPATH}/simu/layers:${NASEPATH}/simu/plasticity:${NASEPATH}/stat:${NASEPATH}/video+tape
+
+
+
