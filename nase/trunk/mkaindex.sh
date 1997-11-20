e310e5a58c7365a9685b43bf3f064314cbda6637
gabriel <gabriel@8aa35c3b-e50b-0410-9e69-e2dc56048116>
1997-11-20 20:16:49 +0000

    Jaja jetzt hat es auch noch einen weissen Hintergrund

--- nase/trunk/mkaindex.sh
+++ nase/trunk/mkaindex.sh
@@ -1,16 +1,28 @@
 #!/usr/ax1301/apps/bash
 DIR=$1
 echo > /tmp/alphrout.html
+echo ' <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2//EN"> 
+<HTML> 
+<HEAD> 
+   <TITLE>IDL-Documenation</TITLE> 
+   <META NAME="GENERATOR" CONTENT="Mozilla/3.0Gold (X11; I; Linux 2.0.30 alpha) [Netscape]"> 
+   <META NAME="Author" CONTENT="Mirko Saam"> 
+</HEAD> 
+<BODY TEXT="#000000" BGCOLOR="#FFFFFF" LINK="#0000EE" VLINK="#551A8B" ALINK="#FF0000"> 
+<P><!-- Changed by: saam, 23-May-1996 --><NEXTID N=A12> 
+<HR></P> 
+<H1>Alphabetical List of Routines</H1><P><UL>' > $DIR/aindex.html
+
 for i in $(find $DIR -name "*.html") ; do
-if [ ! $i = $DIR/aindex.html  ] ; then
+if [ ! $i = $DIR/aindex.html -a ! $i = $DIR/index.html  ] ; then
 PFAD=$(echo $i | sed "s/\//\\\\\//g" )
 grep HREF $i | grep -v Routine | grep  "#" | sed "s/.*<A //g" \
 | tr '[:lower:]' '[:upper:]'\
-| sed "s/#/${PFAD}&/g" | sed "s/^/<P><A /g" | sed "s/$/<\/P>/g" \
+| sed "s/#/${PFAD}&/g" | sed "s/^/<A /g" | sed "s/$/<BR>/g" \
  >>/tmp/alphrout.html
 fi
 done
-sort -t '#' -k 2 /tmp/alphrout.html | uniq > $DIR/aindex.html
-echo "<HR>" dynamical created at $(date) >> $DIR/aindex.html
+sort -t '#' -k 2 /tmp/alphrout.html | uniq >>  $DIR/aindex.html
+echo "</UL></P><HR></BODY></HTML>" dynamical created at $(date) >> $DIR/aindex.html
 rm -f /tmp/alphrout.html
 
