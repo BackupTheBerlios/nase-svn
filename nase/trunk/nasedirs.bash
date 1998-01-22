96d11395e3997f6d5bf9901244cc3126eb2c6c28
saam <saam@8aa35c3b-e50b-0410-9e69-e2dc56048116>
1998-01-22 12:14:05 +0000

    Alien-Dir hinzugefuegt

--- nase/trunk/nasedirs.bash
+++ nase/trunk/nasedirs.bash
@@ -1,4 +1,4 @@
-NASEDIRS=${NASEPATH}/control:${NASEPATH}/graphic:${NASEPATH}/graphic/nonase:${NASEPATH}/input:${NASEPATH}/misc:${NASEPATH}/misc/array:${NASEPATH}/misc/structures:${NASEPATH}/simu/connections:${NASEPATH}/simu:${NASEPATH}/simu/layers:${NASEPATH}/simu/plasticity:${NASEPATH}/stat:${NASEPATH}/video+tape
+NASEDIRS=${NASEPATH}/alien:${NASEPATH}/control:${NASEPATH}/graphic:${NASEPATH}/graphic/nonase:${NASEPATH}/input:${NASEPATH}/misc:${NASEPATH}/misc/array:${NASEPATH}/misc/structures:${NASEPATH}/simu/connections:${NASEPATH}/simu:${NASEPATH}/simu/layers:${NASEPATH}/simu/plasticity:${NASEPATH}/stat:${NASEPATH}/video+tape
 export NASEDIRS
 
 
