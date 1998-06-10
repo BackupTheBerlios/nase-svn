0cbd88f19065e83ae539cc6f1decfe6a5c4951f9
neuroadm <neuroadm@8aa35c3b-e50b-0410-9e69-e2dc56048116>
1998-06-10 15:41:42 +0000

    new hierarchy

--- nase/trunk/nasedirs.bash
+++ nase/trunk/nasedirs.bash
@@ -1,4 +1,6 @@
-NASEDIRS=${NASEPATH}/alien:${NASEPATH}/control:${NASEPATH}/graphic:${NASEPATH}/graphic/nonase:${NASEPATH}/input:${NASEPATH}/misc:${NASEPATH}/misc/array:${NASEPATH}/misc/structures:${NASEPATH}/simu/connections:${NASEPATH}/simu:${NASEPATH}/simu/layers:${NASEPATH}/simu/plasticity:${NASEPATH}/stat:${NASEPATH}/video+tape
+NASEDIRS=${NASEPATH}/alien:${NASEPATH}/control:${NASEPATH}/control/counter:${NASEPATH}/control/loops:${NASEPATH}/control/output:${NASEPATH}/control/time:${NASEPATH}/control/video+tape:${NASEPATH}/graphic:${NASEPATH}/graphic/colors:${NASEPATH}/graphic/nase:${NASEPATH}/graphic/plotcilloscope:${NASEPATH}/graphic/sheets:${NASEPATH}/graphic/support:${NASEPATH}/math:${NASEPATH}/methods:${NASEPATH}/methods/fits:${NASEPATH}/methods/corrs+specs:${NASEPATH}/methods/rfscan:${NASEPATH}/methods/signals:${NASEPATH}/methods/stat:${NASEPATH}/misc:${NASEPATH}/misc/arrays:${NASEPATH}/misc/bugs:${NASEPATH}/misc/files+dirs:${NASEPATH}/misc/handles:${NASEPATH}/misc/keywords:${NASEPATH}/misc/regler:${NASEPATH}/misc/strings:${NASEPATH}/misc/structures:${NASEPATH}/simu:${NASEPATH}/simu/input:${NASEPATH}/simu/connections:${NASEPATH}/simu/layers:${NASEPATH}/simu/plasticity
 export NASEDIRS
+
+
 
 
