6218fb1067d502ad6718f6158995722186589fb6
saam <saam@8aa35c3b-e50b-0410-9e69-e2dc56048116>
2000-11-09 12:01:34 +0000

    dynamic replacement for mainpage.html

---
+++ doc/trunk/www-doc/mainpage.sql
@@ -0,0 +1,44 @@
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML//EN">
+<HTML><HEAD><TITLE>NASE/MIND Documentation System</TITLE>
+<LINK REL="STYLESHEET" HREF="/nase/doc/www-doc/shtml.css">
+</HEAD>
+
+<BODY>
+
+<IMG SRC="/nase/doc/www-doc/nasedoc.gif" HEIGHT=210 WIDTH=320 ALIGN=TEXTTOP>
+
+<BR>
+<BR>
+<UL>
+<LI> <A HREF="/nase/doc/standards/standards.html">NASE/MIND Standards</A> </LI>
+<LI> <A HREF="/nase/doc/workshop/workshop.html">NASE Workshop - Schedule and Proceedings</A> </LI>
+<LI> <A HREF="/nase/doc/workshop/todo.html">To-Do List</A> </LI>
+</UL>
+
+<BR>
+<BR>
+<BR>
+<UL>
+<LI> <A HREF="/nase/doc/standards/cvs.html">CVS Introduction</A> </LI>
+<LI> <A HREF="/nase/doc/simulation/neurontypes.html">NASE Neuron Types</A> </LI>
+</UL>
+<BR>
+<BR>
+<BR>
+
+<! SQL CONNECT localhost chiefnase misfitme>
+<! SQL DATABASE nase >
+
+<! SQL QUERY "select dayofmonth(date),monthname(date),news from news" q1 >
+<! SQL IF $NUM_ROWS != 0 >
+
+<TABLE COLS=2><TR><TH ALIGN=LEFT WIDTH=130>Recent News</TH><TH WIDTH=400></TH>
+<! SQL PRINT_ROWS q1 "<TR><TD CLASS='xmpcode' VALIGN=TOP>@q1.0 @q1.1</TD><TD CLASS="xplcode">@q1.2</TD></TR>\n" >
+</TABLE>
+
+<! sql close>
+
+
+
+</BODY>
+</HTML>
