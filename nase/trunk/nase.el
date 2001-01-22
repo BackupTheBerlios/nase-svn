18ea002d8997fa657d4f71adc4b995502c0b3f35
saam <saam@8aa35c3b-e50b-0410-9e69-e2dc56048116>
2001-01-22 08:56:51 +0000

    another RK bug?

--- nase/trunk/nase.el
+++ nase/trunk/nase.el
@@ -1,6 +1,7 @@
 ;(setq comment-start-skip ";+\\*?[ \t]*")
 
 
+
 ;(defun idlwave-nase-fill-paragraph (&optional nohang)
 ;  "Fills paragraphs in comments.
 ;A paragraph is made up of all contiguous lines having the same comment
