c7b3c1128a88cbef1e84f1fb12b070518b62f23c
ahjthiel <ahjthiel@8aa35c3b-e50b-0410-9e69-e2dc56048116>
2006-02-09 13:01:56 +0000

    Try to execute ScanFile.

--- doc/trunk/www-bin/nasecvscommit.pl
+++ doc/trunk/www-bin/nasecvscommit.pl
@@ -3,27 +3,55 @@
 use lib "$ENV{NASEWWWCOPY}/doc/www-bin/header-parser";
 use lib "$ENV{HOME}/AddPerl/Tie-DBI-1.01/lib";
 use File::Basename;
+use Switch;
 use NASE::parse::parse;
 
 use diagnostics;
 use strict;
 
-my ($dir, @files) = split(" ", $ARGV[0]);
+#my ($dir, @files) = split(" ", $ARGV[0]);
 
 # my $fdir = "/vol/neuro/nase/www-nase-copy/$dir";
-my $fdir = "$ENV{NASEWWWCOPY}/wwwcopy/$dir";
+#my $fdir = "$ENV{NASEWWWCOPY}/wwwcopy/$dir";
 
-my $file;
-foreach $file (@files){
+#my $file;
+#foreach $file (@files){
   # just work on IDL source files
-  if (($file =~ /\.pro$/) && !($dir =~ /neuro/)){
-    if (-r "$fdir/$file"){
-      scanFile(repdir=>$dir, filepath=>"$fdir/$file");
-      print STDERR "$file: updated/inserted using $dir/$file\n";
-    } else {
-      deleteFile(file=>basename($file,""));
-      print STDERR "$file: deleted\n";
-    }    
-  }
-}
+#  if (($file =~ /\.pro$/) && !($dir =~ /neuro/)){
+#    if (-r "$fdir/$file"){
+#      scanFile(repdir=>$dir, filepath=>"$fdir/$file");
+#      print STDERR "$file: updated/inserted using $dir/$file\n";
+#    } else {
+#      deleteFile(file=>basename($file,""));
+#      print STDERR "$file: deleted\n";
+#    }    
+#  }
+#}
+
+my $line;
+while (defined($line = <STDIN>)) {
+  print STDOUT "Piped from svn update:\n $line\n";
+  my @parts = split(" ", $line);
+  switch ($parts[0]) {
+    case "U" {
+	      print "Update:\n";
+	      my $path;
+	      my $base;
+	      my $type;
+	      ($base,$path,$type) = fileparse($parts[1],qr/\.[^.]*/);
+	      print "$path\n";
+	      print "$base\n";
+	      print "$type\n";
+	      if ($type eq ".pro") {
+#		scanFile(repdir=>$dir, filepath=>"$parts[1]");
+		scanFile();#filepath=>"$parts[1]");
+		print "$file: updated/inserted using $parts[1]\n";
+	      } else {
+		print "Not an IDL source file: $type.\n";
+	      }
+	     }
+      else
+	{ print "Unknown: $parts[0]\n"; }
+  }  }
+
 exit 0;
